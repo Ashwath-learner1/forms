d3260c86d93884dbcf1a23bcc93a03e5b931d0f8
Ashwath-learner1 <ashwathkumaruumapathy@gmail.com>
2023-07-24 15:32:00 +0530

    dynamicform

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -5,34 +5,129 @@ const DynamicForm = () => {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [categoryFields, setCategoryFields] = useState([]);
+  const [formData, setFormData] = useState({});
 
+  // Fetch categories when the component mounts
   useEffect(() => {
-    axios.get('http://localhost:5000/api/categories')
+    axios
+      .get('http://localhost:5000/api/categories')
       .then((response) => {
         setCategories(response.data);
-        console.log("data fetched")
       })
       .catch((error) => {
         console.error('Error fetching categories:', error);
       });
   }, []);
 
+  // Fetch fields when a category is selected
   useEffect(() => {
     if (selectedCategory) {
-      axios.get(`http://localhost:5000/api/categories/${selectedCategory}`)
+      axios
+        .get(`http://localhost:5000/api/categories/${selectedCategory}`)
         .then((response) => {
           setCategoryFields(response.data.fields);
+          setFormData({}); // Clear existing form data when category changes
         })
         .catch((error) => {
           console.error('Error fetching category fields:', error);
         });
     } else {
       setCategoryFields([]);
+      setFormData({}); // Clear form data when no category is selected
     }
   }, [selectedCategory]);
 
   const handleCategoryChange = (e) => {
     setSelectedCategory(e.target.value);
+  };
+
+  const toggleCheckboxValue = (fieldName, value, formData) => {
+    const currentData = formData[fieldName] || [];
+    const updatedData = currentData.includes(value)
+      ? currentData.filter((item) => item !== value) // Remove value if it exists
+      : [...currentData, value]; // Add value if it doesn't exist
+    return {
+      ...formData,
+      [fieldName]: updatedData,
+    };
+  };
+  
+  
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+  
+    // Handle checkboxes as an array
+    if (type === 'checkbox') {
+      setFormData((prevFormData) =>
+      toggleCheckboxValue(name, value, prevFormData)
+    );
+    } else if (type === 'radio') {
+      // For radio buttons, we store the value of the selected option
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    } else {
+      // For text and number fields, we store the value directly
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
+  };
+  
+  const renderInputField = (field) => {
+    const { name, type, options } = field;
+
+    switch (type) {
+      case 'checkbox':
+        return (
+          <>
+            {options.map((option) => (
+              <div key={option}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name={name}
+                    value={option}
+                    checked={formData[name]?.includes(option) || false}
+                    onChange={handleInputChange}
+                  />
+                  {option}
+                </label>
+              </div>
+            ))}
+          </>
+        );
+      case 'radio':
+        return (
+          <>
+            {options.map((option) => (
+              <div key={option}>
+                <label>
+                  <input
+                    type="radio"
+                    name={name}
+                    value={option}
+                    checked={formData[name] === option}
+                    onChange={handleInputChange}
+                  />
+                  {option}
+                </label>
+              </div>
+            ))}
+          </>
+        );
+      default:
+        return (
+          <input
+            type={type}
+            name={name}
+            value={formData[name] || ''}
+            onChange={handleInputChange}
+          />
+        );
+    }
   };
 
   return (
@@ -55,11 +150,16 @@ const DynamicForm = () => {
           {categoryFields.map((field) => (
             <div key={field._id}>
               <label htmlFor={field.name}>{field.name}:</label>
-              <input type={field.type} id={field.name} name={field.name} />
+              {renderInputField(field)}
             </div>
           ))}
         </form>
       )}
+
+      <div>
+        <h3>Form Data:</h3>
+        <pre>{JSON.stringify(formData, null, 2)}</pre>
+      </div>
     </div>
   );
 };
